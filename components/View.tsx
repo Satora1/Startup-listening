048489c471cabfb7f7df8543c8512ed0b9376ef3
Jakub Satora <kubasatora@wp.pl>
2024-11-27 13:00:27 +0100

    views format

--- components/View.tsx
+++ components/View.tsx
@@ -4,14 +4,17 @@ import { client } from '@/sanity/lib/client'
 import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
 
 const View = async ({ id }: { id: string }) => {
+
+
     const { views: totalViews } = await client.withConfig({ useCdn: false }).fetch(STARTUP_VIEWS_QUERY, { id })
+
     return (
         <div className='view-container'>
             <div className='absolute -top-2 -right-2'>
                 <Ping />
             </div>
             <p className='view-text'>
-                <span className='font-black'>{totalViews} views</span>
+                <span className='font-black'>{totalViews} {totalViews < 2 ? "view" : "views"}</span>
             </p>
         </div>
     )
