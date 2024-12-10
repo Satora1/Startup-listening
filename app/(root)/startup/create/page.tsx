5c15be0bed09b984d46e7781619e0a52650aaa59
Jakub Satora <kubasatora@wp.pl>
2024-12-10 16:13:36 +0100

    form component

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -1,13 +1,19 @@
+import { auth } from '@/auth'
+import StartupForm from '@/components/StartupForm'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const page = () => {
+const page = async () => {
+    const session=await auth()
+    if(!session)redirect('/')
     return (
         <>
             <section className='pink_container !min-h-[230px]'>
-<h1 className='heading'>
-Sumbit your startup
-</h1>
+                <h1 className='heading'>
+                    Sumbit your startup
+                </h1>
             </section>
+            <StartupForm />
         </>
     )
 }
