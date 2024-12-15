cac8039e4089e06f81e5fad1d8bb44087a7c387e
Jakub Satora <kubasatora@wp.pl>
2024-12-15 13:38:45 +0100

    react issue

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -2,9 +2,11 @@
 import React, { useState } from 'react'
 import { Input } from '@/components/ui/input'
 import { Textarea } from './ui/textarea'
-
+import MDEditor from "@uiw/react-md-editor";
+import { Button } from './ui/button';
 const StartupForm = () => {
     const [errors, setErrors] = useState<Record<string, string>>({})
+    const [pitch,setPtch]=useState()
     return (
         <form action={() => { }} className='startup-form'>
             <div>
@@ -19,16 +21,32 @@ const StartupForm = () => {
                 {errors.description && <p className='startup-form_error'>{errors.description}</p>}
             </div>
             <div>
-                <label htmlFor="category" className='startup-form_label'>Category</label>
-                <Input id="category" name='category' className='startup-form_input' required placeholder='Startup Category' />
-                {errors.category && <p className='startup-form_error'>{errors.category}</p>}
+                <label htmlFor="link" className='startup-form_label'>Image URL</label>
+                <Input id="link" name='link' className='startup-form_input' required placeholder='Startup image URL' />
+                {errors.link && <p className='startup-form_error'>{errors.link}</p>}
             </div>
 
-            <div>
-                <label htmlFor="title" className='startup-form_label'>Title</label>
-                <Input id="title" name='title' className='startup-form_input' required placeholder='Startup Title' />
-                {errors.title && <p className='startup-form_error'>{errors.title}</p>}
+            <div data-color-mode="light">
+                <label htmlFor="pitch" className='startup-form_label'>Title</label>
+               <MDEditor
+               value={pitch}
+               onChange={(value)=>setPtch(value as string) }
+               id="pitch"
+               previev="edit"
+               height={300}
+               style={{borderRadius:20,overflow:"hidden"}}
+               textareaProps={{
+                placeholder:
+                "Briefly describe your idea and what problem it resolve"
+               }}
+               previweOptions={{
+                disallowedElements:["style"],
+               }}
+               />
+                {errors.pitch && <p className='startup-form_error'>{errors.pitch}</p>}
             </div>
+            <Button type="submit" className="startup-form_btn">
+            </Button>
         </form>
     )
 }
