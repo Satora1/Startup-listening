e7375a1d3c6a850cce76bfcffd7e86115f7258ff
Jakub Satora <kubasatora@wp.pl>
2025-01-15 16:17:54 +0100

    some things

--- lib/validation.ts
+++ lib/validation.ts
@@ -3,5 +3,20 @@ import { z } from "zod"
 export const formSchema = z.object({
     title: z.string().min(3).max(100),
     description: z.string().min(20).max(1000),
-
+    category: z.string().min(3).max(20),
+    lin: z.string().url().refine(async (url) => {
+        try {
+            const res = await fetch(url, { method: "HEAD" })
+            const contentType = res.headers.get("content-type")
+            if (contentType?.startsWith("image/")) {
+                return true
+            }
+            else {
+                return false
+            }
+        } catch {
+            return false
+        }
+    }),
+    pitch: z.string().min(10).max(500)
 })
